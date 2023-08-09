3004e37c13398467aab381fff8e36e0abfc6ef81
levanthang2332001 <vanthang23032001@gmail.com>
2023-08-09 14:07:35 +0700

    Initial commit

---
+++ contracts/ITokenProxy.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface ITokenProxy {
+    struct ListRequest {
+        uint256 _price;
+        uint256 _amount;
+        address _from; 
+        address _currency;
+        bytes signature;
+    }
+
+    struct TransferRequest {
+        uint256 _token;
+        uint256 _amount;
+        address _from;
+        address _to;
+    }
+
+    struct MakeOfferRequest {
+        uint256 _price;
+        uint256 _amount;
+        address _currency;
+        uint duration;
+    }
+
+    function buy(ListRequest memory request, uint256 buyAmount) external payable;
+    function makeOffer(MakeOfferRequest memory request) external payable ;
+
+}
